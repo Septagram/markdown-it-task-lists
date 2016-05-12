a70127452cefa86e34de2e11f98c18bde3892066
Igor Novikov <septagramm@gmail.com>
2016-05-13 00:18:47 +0700

    Add options to customize the conversion
    User of the plugin can now customize the classes that will be added to the
    checkbox, containing list item and the list itself, optionally disabling
    them altogether as well. The checkbox can also be enabled (disabled by
    default), so it can be checked/unchecked by user. For deeper
    customization, user can provide their own function to generate
    the checkbox HTML.

--- index.js
+++ index.js
@@ -7,14 +7,38 @@ module.exports = function(md, options) {
 	md.core.ruler.after('inline', 'github-task-lists', function(state) {
 		var tokens = state.tokens;
 		for (var i = 2; i < tokens.length; i++) {
-			if (isTodoItem(tokens, i)) {
+			var todoType = todoItemType(tokens, i);
+			if (todoType !== null) {
 				todoify(tokens[i], state.Token);
-				tokens[i-2].attrSet('class', 'task-list-item');
-				tokens[parentToken(tokens, i-2)].attrSet('class', 'task-list');
+				
+				var itemClass = pluginOptions[todoType ? 'checkedItemClass' : 'uncheckedItemClass'];
+				if (itemClass !== null) {
+					tokens[i-2].attrJoin('class', itemClass);
+				};
+
+				if (pluginOptions.parentClass !== null) {
+					tokens[parentToken(tokens, i-2)].attrJoin('class', pluginOptions.parentClass);
+				};
 			}
 		}
 	});
 };
+
+var pluginOptions = module.exports.options = {
+	checkboxHtml: defaultCheckboxHtml,
+	isCheckboxEnabled: false,
+	checkboxClass: 'task-list-item-checkbox',
+	checkedItemClass: 'task-list-item task-checked',
+	uncheckedItemClass: 'task-list-item',
+	parentClass: 'task-list',
+};
+
+function defaultCheckboxHtml(isChecked) {
+	return 	'<input class="'+pluginOptions.checkboxClass+'" '+
+		(isChecked ? 'checked=""' : '')+' '+
+		(pluginOptions.isCheckboxEnabled ? '' : 'disabled=""')+
+		' type="checkbox">';
+}
 
 function parentToken(tokens, index) {
 	var targetLevel = tokens[index].level - 1;
@@ -26,26 +50,23 @@ function parentToken(tokens, index) {
 	return -1;
 }
 
-function isTodoItem(tokens, index) {
-	return isInline(tokens[index]) &&
-	       isParagraph(tokens[index - 1]) &&
-	       isListItem(tokens[index - 2]) &&
-	       startsWithTodoMarkdown(tokens[index]);
+function todoItemType(tokens, index) {
+	return (isInline(tokens[index]) && isParagraph(tokens[index - 1]) && isListItem(tokens[index - 2]) || null)
+		&& startsWithTodoMarkdown(tokens[index]);
 }
 
 function todoify(token, TokenConstructor) {
-	token.children.unshift(makeCheckbox(token, TokenConstructor));
-	token.children[1].content = token.children[1].content.slice(3);
+	var childToken = token.children[0];
+	if (pluginOptions.checkboxHtml) {
+		token.children.unshift(makeCheckbox(token, TokenConstructor));
+	};
+	childToken.content = childToken.content.slice(3);
 	token.content = token.content.slice(3);
 }
 
 function makeCheckbox(token, TokenConstructor) {
 	var checkbox = new TokenConstructor('html_inline', '', 0);
-	if (token.content.indexOf('[ ]') === 0) {
-		checkbox.content = '<input class="task-list-item-checkbox" disabled="" type="checkbox">';
-	} else if (token.content.indexOf('[x]') === 0 || token.content.indexOf('[X]') === 0) {
-		checkbox.content = '<input class="task-list-item-checkbox" checked="" disabled="" type="checkbox">';
-	}
+	checkbox.content = pluginOptions.checkboxHtml(startsWithTodoMarkdown(token));
 	return checkbox;
 }
 
@@ -55,5 +76,7 @@ function isListItem(token) { return token.type === 'list_item_open'; }
 
 function startsWithTodoMarkdown(token) {
 	// leading whitespace in a list item is already trimmed off by markdown-it
-	return token.content.indexOf('[ ]') === 0 || token.content.indexOf('[x]') === 0 || token.content.indexOf('[X]') === 0;
+	if (token.content.indexOf('[ ]') === 0) return false;
+	if (token.content.indexOf('[x]') === 0 || token.content.indexOf('[X]') === 0) return true;
+	return null;
 }
